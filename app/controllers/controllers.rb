ed022a7a9ea43bd6ee93a66a620d22664f0d2e30
Clarisse Valera <clarisse.valera@gmail.com>
2015-02-12 12:27:14 -0800

    adding navbar w login to layout.erb as well as form for registration

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -1,37 +1,37 @@
-# note that not these aren't all just get routes.
+# note that not these aren"t all just get routes.
 
-get '/' do
+get "/" do
 # login in navbar in layout.erb
 # registration form in home page
   erb :index
 end
 
 # to register
-get '/accounts/new' do
+post "/accounts/new" do
 # lead to profile page
 end
 
 # to login
-get '/accounts/login' do
+post "/accounts/login" do
 # lead to profile page
 end
 
 # route for profile page
-get '/accounts/:id' do
+get "/accounts/:id" do
 # button to take a new survey
 # survey list
 # user stats - V2!
 end
 
-get '/accounts/:id/surveys' do
+get "/accounts/:id/surveys" do
 # list of surveys
 # make new survey option
 end
 
-get '/accounts/:id/surveys/:id' do
+get "/accounts/:id/surveys/:id" do
 # page with description of picked survey
 end
 
-get '/accounts/:id/surveys/take' do
+get "/accounts/:id/surveys/take" do
 # actual link where survey is taken
 end
