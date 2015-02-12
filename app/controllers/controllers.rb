0cb3e39e713ed44663d6c8cbeeb15bcc8740f4a7
Clarisse Valera <clarisse.valera@gmail.com>
2015-02-12 11:45:51 -0800

    routes in controller and included some helpers as well for user sessions

---
+++ app/controllers/controllers.rb
@@ -0,0 +1,36 @@
+# note that not these aren't all just get routes.
+
+get '/' do
+# login in navbar in layout.erb
+# registration form in home page
+end
+
+# to register
+get '/accounts/new' do
+# lead to profile page
+end
+
+# to login
+get '/accounts/login' do
+# lead to profile page
+end
+
+# route for profile page
+get '/accounts/:id' do
+# button to take a new survey
+# survey list
+# user stats - V2!
+end
+
+get '/accounts/:id/surveys' do
+# list of surveys
+# make new survey option
+end
+
+get '/accounts/:id/surveys/:id' do
+# page with description of picked survey
+end
+
+get '/accounts/:id/surveys/take' do
+# actual link where survey is taken
+end
