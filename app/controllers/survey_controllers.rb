0db9d37dfd33f6514dc5b24608b58473d568a252
andy-dev <andres.macedo@gmail.com>
2015-02-12 16:05:40 -0800

    add testing survey

--- app/controllers/survey_controllers.rb
+++ app/controllers/survey_controllers.rb
@@ -1,10 +1,13 @@
 get "/surveys" do
 # list of surveys
 # make new survey option
+erb :survey
 end
 
 get "/surveys/:id" do
 # page with description of picked survey
+# @survey = Survey.find(params[:id])
+
 end
 
 get "/surveys/:id/take" do
