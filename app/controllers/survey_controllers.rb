ab096ab83dfc40d07312cf30599e845c4ffb44fc
andy-dev <andres.macedo@gmail.com>
2015-02-13 10:26:30 -0800

    testing survery

--- app/controllers/survey_controllers.rb
+++ app/controllers/survey_controllers.rb
@@ -11,6 +11,24 @@ get "/surveys/new" do
   erb :newsurvey
 end
 
+post "/surveys/new" do
+  survey_info = params[:survey]
+  user_id = session[:user_id]
+  current_user= User.find(user_id)
+
+  current_user.surveys.create({description: survey_info[:description], name: survey_info[:name] })
+
+  current_survey=current_user.surveys.last
+  current_survey.questions.create({text: survey_info[:question_0]})
+  current_survey_options=
+
+  redirect '/surveys'
+
+end
+
+
+
+
 get "/surveys/:id" do
 # page with description of picked survey
 # @survey = Survey.find(params[:id])
