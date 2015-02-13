7778e1061ca9e5478594ecdba818ee2ccd4e5b6b
Clarisse Valera <clarisse.valera@gmail.com>
2015-02-12 17:45:08 -0800

    add functionality to displays surveys created by user, make survey list clickable so you can access descriptions of each survey, add helper for surveys created, revise width for container in application.css

--- app/controllers/survey_controllers.rb
+++ app/controllers/survey_controllers.rb
@@ -2,12 +2,17 @@ get "/surveys" do
 # list of surveys
   @user = session_current_user
   @all_surveys = Survey.all
+  @surveys_created = surveys_created
   @survey_titles = names_of_surveys_taken
+  p Survey.where(user_id: session_current_user.id)
   erb :survey_list
 end
 
 get "/surveys/:id" do
 # page with description of picked survey
+  @survey = Survey.find(params[:id])
+  @num_of_questions = @survey.questions.count
+  erb :survey_description
 end
 
 get "/surveys/:id/take" do
