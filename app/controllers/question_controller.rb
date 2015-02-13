5b0e90719a5cba7c37ec907e3d31735aa46f1c81
Peter Haugen <phaugen@gmail.com>
2015-02-13 11:59:44 -0800

    answering questions works, selecting questions works, finishing survey works, need to implement survey/results route.

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -9,7 +9,17 @@ post "/surveys/:sid/question/:qid" do |s_id, q_id|
   answer_info.each do |option_id, response|
     Selection.create({user_id: user_id, survey_id: s_id, option_id: option_id, response: response})
   end
-
+  questions_left = Selection.unanswered_questions({user_id: user_id, survey_id: s_id})
+  p questions_left
+  puts
+  if questions_left.any?
+    next_question = questions_left.sample
+    p next_question
+    puts
+    redirect ("/surveys/%d/question/%d" % [s_id, next_question.id])
+  else
+    redirect("/surveys/%d/results" % s_id)
+  end
   # send off to next answer somehow, sorry we didn't get to it.
 
 end
--- app/models/selection.rb
+++ app/models/selection.rb
@@ -1,3 +1,16 @@
 class Selection < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :user
+  belongs_to :survey
+  belongs_to :option
+
+  def self.unanswered_questions(args)
+    user = User.find(args[:user_id])
+    survey = Survey.find(args[:survey_id])
+    survey_questions = survey.questions
+    taken_options = user.options
+    # answered_questions = taken_options.map{ |option| option.question}
+    answered_questions = taken_options.map(&:question)
+    return survey_questions-answered_questions
+  end
 end
